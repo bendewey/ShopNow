c5cfaf9fe389e5247dcc2292ae6b0a4143119bc0
Ben Dewey <bdewey01@hotmail.com>
2013-03-26 18:48:28 -0400

    Cleaned usings

--- ShopNow/Services/ToastNotificationService.cs
+++ ShopNow/Services/ToastNotificationService.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NotificationsExtensions.ToastContent;
 using ShopNow.Events;
 using Windows.UI.Notifications;
