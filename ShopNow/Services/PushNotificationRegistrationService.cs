01a68be408858357a6ca2478faf4dc328ee1e70f
Ben Dewey <bdewey01@hotmail.com>
2013-03-26 10:15:25 -0400

    Refactored push notification dialog to use Roaming Settings

--- ShopNow/Services/PushNotificationRegistrationService.cs
+++ ShopNow/Services/PushNotificationRegistrationService.cs
@@ -4,6 +4,7 @@ using ShopNow.DataModel;
 using Microsoft.WindowsAzure.MobileServices;
 using ShopNow.DataModel.Repository;
 using Windows.Networking.PushNotifications;
+using Windows.Storage;
 using Windows.UI.Popups;
 
 namespace ShopNow.Services
@@ -36,22 +37,37 @@ namespace ShopNow.Services
 
         private async void RequestPermissionToPushDeals(PushNotificationChannel channel)
         {
-            if (!SuspensionManager.SessionState.ContainsKey(SuspensionManagerConstants.AcceptPushDeals))
+            if (!HasSetting(SuspensionManagerConstants.AcceptPushDeals))
             {
-                var dialog = new MessageDialog("Would you like to enable push notification for deals and specials?");
+                var dialog = new MessageDialog("Would you like to enable push notification for deals and specials?   You can change this via settings later.");
                 dialog.Commands.Add(new UICommand("Yes"));
                 dialog.Commands.Add(new UICommand("No"));
                 var command = await dialog.ShowAsync();
-                SuspensionManager.SessionState.Add(SuspensionManagerConstants.AcceptPushDeals, command.Label == "Yes");
+                SetSetting(SuspensionManagerConstants.AcceptPushDeals, command.Label == "Yes");
             }
 
-            if ((bool)SuspensionManager.SessionState[SuspensionManagerConstants.AcceptPushDeals] == false)
+            if (GetSetting(SuspensionManagerConstants.AcceptPushDeals) == false)
             {
                 return;
             }
 
             var repository = ServiceLocator.Get<PushNotificationDealsSubscribersRepository>();
             repository.UpdateOrInsertDeviceSubscription(HardwareIdentificationHelpers.GetHardwareId(), channel.Uri, channel.ExpirationTime);
+        }
+
+        public bool HasSetting(string key)
+        {
+            return ApplicationData.Current.RoamingSettings.Values.ContainsKey(key);
+        }
+
+        public void SetSetting(string key, bool value)
+        {
+            ApplicationData.Current.RoamingSettings.Values.Add(key, value);
+        }
+
+        public bool GetSetting(string key)
+        {
+            return (bool)ApplicationData.Current.RoamingSettings.Values[key];
         }
     }
 }
